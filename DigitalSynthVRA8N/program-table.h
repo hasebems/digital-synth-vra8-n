e12d0a946c8a037f756b6397c33335b07c1b70a6
Hasebe Masahiko <JCA03205@gmail.com>
2019-07-18 23:19:43 +0900

    プリセットデータ差し替え
    SAXduinoのプリセットデータに差し替え、31250bpsに修正

--- DigitalSynthVRA8N/program-table.h
+++ DigitalSynthVRA8N/program-table.h
@@ -2,106 +2,49 @@
 
 const uint8_t PROGRAM_NUMBER_DEFAULT = 0;
 
-#if !defined(ENABLE_VOLTAGE_CONTROL) || !defined(USE_PITCH_CV_IN)
+// PRESET for SAXduino                            #0   #1   #2   #3   #4   #5             
+//                                                SAX  CLA  FLU  SAW  SQU  NEUTRAL        
+const uint8_t g_preset_table_OSC1_2_WAVE    [] = {0  , 127, 127, 0  , 127, 0  , 0  , 0  };
+const uint8_t g_preset_table_OSC_MIX        [] = {0  , 0  , 0  , 32 , 64 , 0  , 0  , 0  };
+const uint8_t g_preset_table_OSC2_COARSE    [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
+const uint8_t g_preset_table_OSC2_FINE      [] = {72 , 72 , 72 , 48 , 72 , 72 , 72 , 72 };
 
-// PRESET                                         #0   #1   #2   #3   #4   #5   #6   #7   
-const uint8_t g_preset_table_OSC1_2_WAVE    [] = {0  , 0  , 0  , 127, 127, 0  , 48 , 0  };
-const uint8_t g_preset_table_OSC_MIX        [] = {64 , 64 , 64 , 64 , 0  , 32 , 127, 0  };
-const uint8_t g_preset_table_OSC2_COARSE    [] = {71 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
-const uint8_t g_preset_table_OSC2_FINE      [] = {68 , 72 , 66 , 72 , 72 , 48 , 68 , 64 };
+const uint8_t g_preset_table_SUB_WAVE       [] = {64 , 64 , 64 , 127, 64 , 64 , 64 , 64 };
+const uint8_t g_preset_table_SUB_LEVEL      [] = {0  , 0  , 8  , 0  , 0  , 0  , 0  , 0  };
+const uint8_t g_preset_table_EG_TO_PITCH    [] = {64 , 64 , 64 , 80 , 80 , 64 , 64 , 64 };
+const uint8_t g_preset_table_EG_TO_P_TGT    [] = {0  , 0  , 0  , 127, 0  , 0  , 0  , 0  };
 
-const uint8_t g_preset_table_SUB_WAVE       [] = {127, 0  , 0  , 0  , 64 , 127, 0  , 0  };
-const uint8_t g_preset_table_SUB_LEVEL      [] = {127, 127, 127, 0  , 8  , 0  , 0  , 0  };
-const uint8_t g_preset_table_EG_TO_PITCH    [] = {64 , 62 , 64 , 64 , 64 , 80 , 120, 64 };
-const uint8_t g_preset_table_EG_TO_P_TGT    [] = {0  , 127, 0  , 0  , 0  , 127, 0  , 0  };
+const uint8_t g_preset_table_CUTOFF         [] = {100, 64 , 0  , 100, 127, 127, 127, 127};
+const uint8_t g_preset_table_RESONANCE      [] = {0  , 0  , 0  , 64 , 0  , 0  , 0  , 0  };
+const uint8_t g_preset_table_EG_TO_CUTOFF   [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
+const uint8_t g_preset_table_AMP_EG         [] = {127, 127, 127, 127, 0  , 127, 127, 127};
 
-const uint8_t g_preset_table_CUTOFF         [] = {64 , 16 , 16 , 112, 0  , 100, 64 , 127};
-const uint8_t g_preset_table_RESONANCE      [] = {96 , 80 , 127, 0  , 0  , 64 , 64 , 0  };
-const uint8_t g_preset_table_EG_TO_CUTOFF   [] = {82 , 112, 94 , 64 , 64 , 64 , 64 , 64 };
-const uint8_t g_preset_table_AMP_EG         [] = {127, 0  , 127, 0  , 127, 127, 127, 127};
+const uint8_t g_preset_table_ATTACK         [] = {16 , 16 , 32 , 16 , 0  , 0  , 0  , 0  };
+const uint8_t g_preset_table_DECAY          [] = {32 , 32 , 32 , 32 , 16 , 0  , 0  , 0  };
+const uint8_t g_preset_table_SUSTAIN        [] = {96 , 127, 127, 96 , 0  , 127, 127, 127};
+const uint8_t g_preset_table_RELEASE        [] = {0  , 127, 127, 0  , 0  , 127, 127, 127};
 
-const uint8_t g_preset_table_ATTACK         [] = {0  , 0  , 0  , 0  , 32 , 16 , 0  , 0  };
-const uint8_t g_preset_table_DECAY          [] = {64 , 96 , 96 , 96 , 32 , 32 , 64 , 0  };
-const uint8_t g_preset_table_SUSTAIN        [] = {127, 0  , 0  , 0  , 127, 96 , 0  , 127};
-const uint8_t g_preset_table_RELEASE        [] = {127, 127, 127, 127, 127, 0  , 127, 127};
+const uint8_t g_preset_table_LFO_WAVE       [] = {32 , 32 , 32 , 32 , 32 , 32 , 32 , 32 };
+const uint8_t g_preset_table_LFO_RATE       [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
+const uint8_t g_preset_table_LFO_TO_PITCH   [] = {66 , 66 , 66 , 66 , 66 , 66 , 66 , 66 };
+const uint8_t g_preset_table_LFO_TO_CUTOFF  [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
 
-const uint8_t g_preset_table_LFO_WAVE       [] = {0  , 0  , 32 , 127, 32 , 32 , 0  , 0  };
-const uint8_t g_preset_table_LFO_RATE       [] = {64 , 64 , 64 , 80 , 64 , 64 , 0  , 64 };
-const uint8_t g_preset_table_LFO_TO_PITCH   [] = {66 , 66 , 64 , 88 , 66 , 66 , 64 , 64 };
-const uint8_t g_preset_table_LFO_TO_CUTOFF  [] = {64 , 64 , 40 , 64 , 64 , 64 , 112, 64 };
+const uint8_t g_preset_table_LFO_DEPTH      [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
+const uint8_t g_preset_table_EG_TO_LFO_RATE [] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
+const uint8_t g_preset_table_LFO_TO_P_TGT   [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
+const uint8_t g_preset_table_LFO_FADE_TIME  [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 
-const uint8_t g_preset_table_LFO_DEPTH      [] = {8  , 8  , 0  , 0  , 8  , 8  , 0  , 0  };
-const uint8_t g_preset_table_EG_TO_LFO_RATE [] = {64 , 64 , 64 , 72 , 64 , 64 , 64 , 64 };
-const uint8_t g_preset_table_LFO_TO_P_TGT   [] = {127, 0  , 0  , 0  , 0  , 0  , 0  , 0  };
-const uint8_t g_preset_table_LFO_FADE_TIME  [] = {64 , 64 , 0  , 0  , 64 , 64 , 0  , 0  };
+const uint8_t g_preset_table_P_BEND_RANGE   [] = {2  , 2  , 2  , 2  , 2  , 2  , 2  , 2  };
+const uint8_t g_preset_table_PITCH_TO_CUTOFF[] = {127, 64 , 127, 127, 64 , 127, 127, 127};
+const uint8_t g_preset_table_EXP_TO_CUTOFF  [] = {88 , 88 , 88 , 112, 64 , 88 , 88 , 88 };
+const uint8_t g_preset_table_EXP_TO_AMP_LEV [] = {127, 127, 127, 127, 127, 127, 127, 127};
 
-const uint8_t g_preset_table_P_BEND_RANGE   [] = {2  , 2  , 2  , 2  , 2  , 2  , 12 , 2  };
-const uint8_t g_preset_table_PITCH_TO_CUTOFF[] = {0  , 0  , 0  , 0  , 127, 127, 127, 0  };
-const uint8_t g_preset_table_EXP_TO_CUTOFF  [] = {64 , 64 , 64 , 64 , 88 , 112, 64 , 64 };
-const uint8_t g_preset_table_EXP_TO_AMP_LEV [] = {0  , 0  , 0  , 0  , 127, 127, 127, 0  };
+const uint8_t g_preset_table_PORTAMENTO     [] = {0  , 0  , 0  , 0  , 32 , 0  , 0  , 0  };
+const uint8_t g_preset_table_LEGATO         [] = {127, 127, 127, 127, 127, 127, 127, 127};
+const uint8_t g_preset_table_KEY_ASSIGN     [] = {127, 127, 127, 127, 127, 127, 127, 127};
+const uint8_t g_preset_table_EXP_BY_VEL     [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  };
 
-const uint8_t g_preset_table_PORTAMENTO     [] = {0  , 32 , 32 , 0  , 0  , 0  , 0  , 0  };
-const uint8_t g_preset_table_LEGATO         [] = {0  , 127, 127, 0  , 127, 127, 0  , 0  };
-const uint8_t g_preset_table_KEY_ASSIGN     [] = {0  , 64 , 0  , 127, 127, 127, 127, 0  };
-const uint8_t g_preset_table_EXP_BY_VEL     [] = {0  , 0  , 0  , 0  , 0  , 0  , 127, 0  };
-
-const uint8_t g_preset_table_OSC_LEVEL      [] = {80 , 80 , 80 , 80 , 120, 107, 80 , 80 };
-const uint8_t g_preset_table_RESONANCE_LIMIT[] = {127, 127, 127, 127, 96 , 96 , 127, 127};
+const uint8_t g_preset_table_OSC_LEVEL      [] = {107, 120, 120, 107, 80 , 80 , 80 , 80 };
+const uint8_t g_preset_table_RESONANCE_LIMIT[] = {96 , 96 , 96 , 96 , 127, 127, 127, 127};
 const uint8_t g_preset_table_AMP_LEVEL      [] = {127, 127, 127, 127, 127, 127, 127, 127};
-const uint8_t g_preset_table_DAMP_AND_ATTACK[] = {0  , 0  , 0  , 0  , 64 , 64 , 0  , 0  };
-
-
-
-
-
-
-
-#else  // defined(ENABLE_VOLTAGE_CONTROL) && defined(USE_PITCH_CV_IN)
-
-// PRESET for VRA8-N mode-VC                      #0   #1   #2   #3   #4   #5   #6   #7   
-const uint8_t g_preset_table_OSC1_2_WAVE    [] = {0  , 0  , 0  , 0  , 127, 127, 0  , 48 };
-const uint8_t g_preset_table_OSC_MIX        [] = {64 , 64 , 64 , 64 , 64 , 0  , 32 , 127};
-const uint8_t g_preset_table_OSC2_COARSE    [] = {64 , 71 , 64 , 64 , 64 , 64 , 64 , 64 };
-const uint8_t g_preset_table_OSC2_FINE      [] = {68 , 68 , 72 , 66 , 72 , 72 , 48 , 68 };
-
-const uint8_t g_preset_table_SUB_WAVE       [] = {0  , 127, 0  , 0  , 0  , 64 , 127, 0  };
-const uint8_t g_preset_table_SUB_LEVEL      [] = {127, 127, 127, 127, 0  , 8  , 0  , 0  };
-const uint8_t g_preset_table_EG_TO_PITCH    [] = {64 , 64 , 62 , 64 , 64 , 64 , 80 , 120};
-const uint8_t g_preset_table_EG_TO_P_TGT    [] = {0  , 0  , 127, 0  , 0  , 0  , 127, 0  };
-
-const uint8_t g_preset_table_CUTOFF         [] = {64 , 64 , 16 , 16 , 112, 0  , 100, 64 };
-const uint8_t g_preset_table_RESONANCE      [] = {64 , 96 , 80 , 127, 0  , 0  , 64 , 64 };
-const uint8_t g_preset_table_EG_TO_CUTOFF   [] = {64 , 82 , 112, 94 , 64 , 64 , 64 , 64 };
-const uint8_t g_preset_table_AMP_EG         [] = {127, 127, 0  , 127, 0  , 127, 127, 127};
-
-const uint8_t g_preset_table_ATTACK         [] = {0  , 0  , 0  , 0  , 0  , 32 , 16 , 0  };
-const uint8_t g_preset_table_DECAY          [] = {64 , 64 , 96 , 96 , 96 , 32 , 32 , 64 };
-const uint8_t g_preset_table_SUSTAIN        [] = {127, 127, 0  , 0  , 0  , 127, 96 , 0  };
-const uint8_t g_preset_table_RELEASE        [] = {127, 127, 127, 127, 127, 127, 0  , 127};
-
-const uint8_t g_preset_table_LFO_WAVE       [] = {0  , 0  , 0  , 32 , 127, 32 , 32 , 0  };
-const uint8_t g_preset_table_LFO_RATE       [] = {64 , 64 , 64 , 64 , 80 , 64 , 64 , 0  };
-const uint8_t g_preset_table_LFO_TO_PITCH   [] = {65 , 66 , 66 , 64 , 88 , 66 , 66 , 64 };
-const uint8_t g_preset_table_LFO_TO_CUTOFF  [] = {64 , 64 , 64 , 40 , 64 , 64 , 64 , 112};
-
-const uint8_t g_preset_table_LFO_DEPTH      [] = {16 , 8  , 8  , 0  , 0  , 8  , 8  , 0  };
-const uint8_t g_preset_table_EG_TO_LFO_RATE [] = {64 , 64 , 64 , 64 , 72 , 64 , 64 , 64 };
-const uint8_t g_preset_table_LFO_TO_P_TGT   [] = {0  , 127, 0  , 0  , 0  , 0  , 0  , 0  };
-const uint8_t g_preset_table_LFO_FADE_TIME  [] = {0  , 64 , 64 , 0  , 0  , 64 , 64 , 0  };
-
-const uint8_t g_preset_table_P_BEND_RANGE   [] = {30 , 2  , 2  , 2  , 2  , 2  , 2  , 12 };
-const uint8_t g_preset_table_PITCH_TO_CUTOFF[] = {0  , 0  , 0  , 0  , 0  , 127, 127, 127};
-const uint8_t g_preset_table_EXP_TO_CUTOFF  [] = {64 , 64 , 64 , 64 , 64 , 88 , 112, 64 };
-const uint8_t g_preset_table_EXP_TO_AMP_LEV [] = {0  , 0  , 0  , 0  , 0  , 127, 127, 127};
-
-const uint8_t g_preset_table_PORTAMENTO     [] = {0  , 0  , 32 , 32 , 0  , 0  , 0  , 0  };
-const uint8_t g_preset_table_LEGATO         [] = {0  , 0  , 127, 127, 0  , 127, 127, 0  };
-const uint8_t g_preset_table_KEY_ASSIGN     [] = {127, 0  , 64 , 0  , 127, 127, 127, 127};
-const uint8_t g_preset_table_EXP_BY_VEL     [] = {0  , 0  , 0  , 0  , 0  , 0  , 0  , 127};
-
-const uint8_t g_preset_table_OSC_LEVEL      [] = {80 , 80 , 80 , 80 , 80 , 120, 107, 80 };
-const uint8_t g_preset_table_RESONANCE_LIMIT[] = {127, 127, 127, 127, 127, 96 , 96 , 127};
-const uint8_t g_preset_table_AMP_LEVEL      [] = {127, 127, 127, 127, 127, 127, 127, 127};
-const uint8_t g_preset_table_DAMP_AND_ATTACK[] = {0  , 0  , 0  , 0  , 0  , 64 , 64 , 0  };
-
-#endif
+const uint8_t g_preset_table_DAMP_AND_ATTACK[] = {64 , 64 , 64 , 64 , 64 , 64 , 64 , 64 };
